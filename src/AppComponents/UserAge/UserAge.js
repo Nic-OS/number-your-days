75cfd6958a8a51ff286debea68908d75169a76ef
Nico Salas <salasn24@gmail.com>
2018-04-06 07:35:02 -0400

    minimal styling, added comments

--- src/AppComponents/UserAge/UserAge.js
+++ src/AppComponents/UserAge/UserAge.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { View, TextInput, Text, Button} from 'react-native';
+import { View, TextInput, Text, TouchableOpacity } from 'react-native';
 import { connect } from 'react-redux';
 import { setUserAge } from '../../Redux/actions';
 
@@ -24,26 +24,58 @@ class UserAge extends Component {
   render() {
 
     return (
-      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center', backgroundColor: 'white' }}>
-        <Text style={{fontSize: 20, fontWeight: 'bold'}}>
-          Please enter your age below.
-        </Text>
-        <TextInput
-          value={this.state.age}
-          placeholder='Enter age here:'
-          keyboardType='numeric'
-          onChangeText={this.onChangeText}
-          onSubmitEditing={this.onSubmit}
-        />
-        <Button
-          title="Next"
-          onPress={() => this.props.navigation.navigate('Gender')}
-        />
+      <View style={{ flex: 1, justifyContent: 'flex-start', backgroundColor: 'white', alignItems: 'center' }}>
+        <View style={{ width: 400, height: 200, justifyContent: 'center', alignItems: 'center' }}>
+          <Text style={{fontSize: 45, fontWeight: 'bold', color: 'black'}}>
+            What's your age?
+          </Text>
+        </View>
+        <View style={{height: 200, width: 200, marginTop: 20, justifyContent: 'center' }}>
+          <TextInput
+            value={this.state.age}
+            placeholder='Enter age here:'
+            keyboardType='numeric'
+            enablesReturnKeyAutomatically={true} //iOS only
+            onChangeText={this.onChangeText}
+            onSubmitEditing={this.onSubmit}
+          />
+        </View>
+        <View style={{ marginTop: 20 }}>
+          <TouchableOpacity
+            style={{padding: 20, marginLeft: 75, marginRight: 75, backgroundColor: 'skyblue', alignItems: 'center'}}
+            onPress={() => this.props.navigation.navigate('Gender')}>
+            <Text style={{fontWeight: 'bold', fontSize: 20}}>
+              Next
+            </Text>
+          </TouchableOpacity>
+        </View>
       </View>
     )
   }
 }
 
+
+/*
+React-Redux stuff. The following three functions "connect" the child component to Redux by creating a wrapper 
+component that's able to access and modify the state in the Redux store. 
+
+`mapStateToProps()` accepts the current state tree in the Redux store and allows you 
+to map a piece of the state to this child component's props. Then you can use those props to either display or 
+modify the value of the state through action creators, which brings me to the next function.
+
+`mapDispatchToProps` simplifies the process of dispatching your action creators to the reducer(s) when you want
+to update the state. After importing an action creator in your component, this function allows you to map a 
+DISPATCHED version of the action creator to your props. This means that your props, when called, will dispatch 
+their returned value (the action itself) to the reducer, thereby updating the state. Easy peasy.
+
+`connect()` does all the hard work of creating the abstraction layer on your actual component that can access
+Redux. It combines what was returned from the first two functions and your actual component and creates a new
+wrapper component that's connected to Redux. This promotes reusability of your components while allowing
+you to easily connect to Redux when necessary.
+*/
+
+// Accepts current Redux store state tree. Maps the piece (or pieces) of state you want onto props to be used in
+// your component. `age` attribute key represents prop name. `state.age` represents piece of state to map.
 const mapStateToProps = state => {
   return {
     age: state.age
@@ -52,10 +84,9 @@ const mapStateToProps = state => {
 
 // Anything returned from this function will end up as props in the UserAge component.
 const mapDispatchToProps = dispatch => ({
-  //Whenever setUserAge is called, the result should be passed to all reducers.
+  // Whenever setUserAge is called, the result will be passed to all reducers.
   setUserAge: value => dispatch(setUserAge(value))
 })
 
-// Promote UserAge from 'dumb' component to 'smart' container - it needs to know
-// about the new dispatch method, setUserAge, and make it available as a prop.
+// Promote UserAge from 'dumb' component to 'smart' container that's connected to Redux.
 export default connect(mapStateToProps, mapDispatchToProps)(UserAge);
--- src/AppComponents/Welcome/Welcome.js
+++ src/AppComponents/Welcome/Welcome.js
@@ -8,7 +8,7 @@ render() {
   return(
     <View style={{flex: 1, alignItems: 'center', justifyContent: 'center', backgroundColor: 'white'}}>
       <View style={{height: 200, width: 400, justifyContent: 'center'}}>
-        <Text style={{fontSize: 35, fontWeight: 'bold', textAlign: 'center'}}>
+        <Text style={{color: 'black', fontSize: 35, fontWeight: 'bold', textAlign: 'center'}}>
           Welcome to the Number Your Days app.
         </Text>
       </View>
@@ -16,8 +16,8 @@ render() {
         <TouchableOpacity
           style={{padding: 20, marginLeft: 75, marginRight: 75, backgroundColor: 'skyblue', alignItems: 'center'}}
           onPress={() => this.props.navigation.navigate('Age')}>
-          <Text style={{fontWeight: 'bold', fontSize: 20}}>
-            Click here to begin
+          <Text style={{fontWeight: 'bold', color: 'black', fontSize: 20}}>
+            Let's begin
           </Text>
         </TouchableOpacity>
       </View>
